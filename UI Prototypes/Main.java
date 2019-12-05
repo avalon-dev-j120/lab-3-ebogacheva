c52174c03a3de4dd336e6ba90d5085e843b3935a
JAVA <JAVA@WS-248-03.avalon.ru>
2019-12-05 21:17:30 +0300

    gridbagconstraints

---
+++ UI Prototypes/Main.java
@@ -0,0 +1,11 @@
+
+import color_selection.ColorSelectionForm;
+
+
+class Main { 
+
+    public static void main(String[] args) { 
+     
+        ColorSelectionForm form = new ColorSelectionForm();
+    } 
+} 
