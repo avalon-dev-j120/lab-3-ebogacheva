1f25615393584c5ad92c9129503aa25bb7c63adf
Ekaterina Bogacheva <bogacheva.e@gmail.com>
2019-12-08 01:42:31 +0300

    fonts, ticks, etc.

--- Applications/src/ColorPalette.java
+++ Applications/src/ColorPalette.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.Dictionary;
+import java.util.Hashtable;
 
 public class ColorPalette extends JFrame {
 
@@ -14,6 +16,8 @@ public class ColorPalette extends JFrame {
     private JSlider sliderGreen;
     private JSlider sliderBlue;
     private JPanel sliderSet;
+
+    private final Font labelFont = new Font("Arial", Font.PLAIN, 12);
 
 
     public ColorPalette() {
@@ -47,9 +51,13 @@ public class ColorPalette extends JFrame {
 
         // Initialization of the LabelSet
         {
+
             labelRed = new JLabel("Red:");
+            labelRed.setFont(labelFont);
             labelGreen = new JLabel("Green:");
+            labelGreen.setFont(labelFont);
             labelBlue = new JLabel("Blue:");
+            labelBlue.setFont(labelFont);
 
             labelSet = new JPanel();
             GridLayout labelSetLayout = new GridLayout(3, 1);
@@ -63,7 +71,7 @@ public class ColorPalette extends JFrame {
             constraintsLabelSet.gridy = 0;
             constraintsLabelSet.weightx = 0.5;
             constraintsLabelSet.weighty = 2;
-            constraintsLabelSet.insets = new Insets(5, 5, 5, 5);
+            constraintsLabelSet.insets = new Insets(0, 5, 25, 5);
             constraintsLabelSet.anchor = GridBagConstraints.LINE_START;
             constraintsLabelSet.fill = GridBagConstraints.BOTH;
 
@@ -73,13 +81,37 @@ public class ColorPalette extends JFrame {
         // Initialization of the Slider Set
         {
             sliderRed = new JSlider(0, 255,125);
-            sliderRed.setMajorTickSpacing(18);
+            sliderRed.setMajorTickSpacing(17);
+            sliderRed.setPaintLabels(true);
+            sliderRed.setFont(labelFont);
+            Dictionary dictionary = sliderRed.getLabelTable();
+            int i = 17;
+            while (i < 255){
+                dictionary.remove(i);
+                i = i + 17;
+            }
             sliderRed.setPaintTicks(true);
             sliderGreen = new JSlider(0, 255, 125);
-            sliderGreen.setMajorTickSpacing(18);
+            sliderGreen.setMajorTickSpacing(17);
+            sliderGreen.setPaintLabels(true);
+            sliderGreen.setFont(labelFont);
+            Dictionary dictionary2 = sliderGreen.getLabelTable();
+            int i2 = 17;
+            while (i2 < 255){
+                dictionary2.remove(i2);
+                i2 = i2 + 17;
+            }
             sliderGreen.setPaintTicks(true);
             sliderBlue = new JSlider(0, 255, 125);
-            sliderBlue.setMajorTickSpacing(18);
+            sliderBlue.setMajorTickSpacing(17);
+            sliderBlue.setFont(labelFont);
+            sliderBlue.setPaintLabels(true);
+            Dictionary dictionary3 = sliderBlue.getLabelTable();
+            int i3 = 17;
+            while (i3 < 255){
+                dictionary3.remove(i3);
+                i3 = i3 + 17;
+            }
             sliderBlue.setPaintTicks(true);
 
             sliderSet = new JPanel();
